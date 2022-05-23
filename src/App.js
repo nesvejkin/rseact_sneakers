ef36c80cff0685481589c6b8768d4fd95e895f76
nesvejkin <nesvejkin@gmail.com>
2022-05-23 16:11:24 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function App() {
-  return <div className="App">dbbddb</div>;
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div>
+            <h3>React Sneacers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/kar.svg" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ...........
+      </div>
+    </div>
+  );
 }
 
 export default App;
